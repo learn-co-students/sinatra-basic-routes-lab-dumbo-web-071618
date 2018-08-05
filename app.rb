f5c1139ea182b1d35a2c4bd71001dcffbe5dab62
scarlettperry <si.perry15@gmail.com>
2018-08-05 12:29:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Scarlett."
+  end
+
+  get "/hometown" do
+    "My hometown is Bronx, NY."
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'Get Free' by Major Lazer and Amber Coffman."
+  end
+
 end
