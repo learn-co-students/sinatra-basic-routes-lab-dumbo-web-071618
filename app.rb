685c4c7617e0d9c6a89c2eeeae3537aa6a4a1290
Kyle-Neale <kyle.a.neale@gmail.com>
2018-08-06 13:58:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kyle"
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Favorite Song - Chance the Rapper"
+  end
 end
